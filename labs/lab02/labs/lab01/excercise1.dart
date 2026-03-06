55c06bdaaeaa2c3535efae17fa2507c13edf64d9
Nicola Bucci <buccinicola7@gmail.com>
2026-03-06 21:04:41 +0100

    [main] remove duplicated labs folder from lab02

--- labs/lab02/labs/lab01/excercise1.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-void main(List<String> args) {
-    int n = 5;
-    for (var i = 0; i < n; i++) {
-        print(pow(i,4));
-    }
-
-}
--- labs/lab02/labs/lab01/exercise2.dart
+++
@@ -1,8 +0,0 @@
-
-void main(List<String> args) {
-  var list = [2,4,6,8,10]; 
-  final mappedList = list.map((element){
-    return element*2;
-  });
-  print('$mappedList');
-}
